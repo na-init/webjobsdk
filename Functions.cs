0bb578f72d493d7d739c3f6983738adf161c528e
Naushad Alam <30662331+na-init@users.noreply.github.com>
2022-12-16 08:38:37 +0000

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobsSDKSample
+{
+    public class Functions
+    {
+        
+
+        [FunctionName("TimerFunction")]
+        public async Task RunAsyn([TimerTrigger ("%cronsetting%")] TimerInfo myTimer, ILogger log)
+        {
+            
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobsSDKSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath (Directory.GetCurrentDirectory())
+                .AddJsonFile ("appsettings.json", optional: false, reloadOnChange: true)
+                .Build ();
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddTimers();
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+
+            });
+            
+            var host = builder.Build();
+            using (host)
+            {
+                var jobHost = host.Services.GetService<IJobHost>();
+                await host.StartAsync();
+               await jobHost.CallAsync("TimerFunction");
+                await host.StopAsync();
+                //host.Run();
+            }
+        }
+    }
+}
